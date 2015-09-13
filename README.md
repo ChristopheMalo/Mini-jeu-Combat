70ddf57f825b3dda13778310516a2c2f7dc7ea4a
ChristopheMalo <christof.malo@gmail.com>
2015-09-13 20:18:14 +0200

    Initialiser attributs et méthodes de la classe personnage

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Développement d'un mini-jeu de combat en POO et PHP
 ## Descriptif du projet
 - Conception du projet
 - Création de la BDD
+- Création de la classe personnage
 
 ## Copyright
 Openclassrooms - Adaptation Christophe Malo
---
+++ classes/Personnage.php
@@ -0,0 +1,87 @@
+<?php
+class Personnage {
+    /*
+     * Attributs
+     */
+    private $_id;
+    private $_nom;
+    private $_degats;
+    
+    
+    /*
+     * Déclaration des constantes
+     */
+    const DETECT_ME     = 1;
+    const PERSO_DEAD    = 2;
+    const PERSO_COUP    = 3;
+
+
+    /*
+     * Méthode de construction
+     */
+    public function __construct($param) {
+        
+    }
+    
+    
+    /*
+     * Methode d'hydratation
+     */
+    public function hydrate($param) {
+        
+    }
+    
+    
+    /*
+     * Méthodes génériques
+     */
+    // Methode de gestion de la frappe d'un personnage sur un autre
+    public function frapper(Personnage $persoAFrapper) {
+        
+    }
+    
+    // Methode de gestion de réception d'un coup, d'un dégat
+    // Augmentation des dégats par 10 - à 100 de dégats ou plus le personnage est mort
+    public function recevoirUnCoup($param) {
+        
+    }
+    
+    
+    /*
+     * Méthodes Accesseurs (Getters) - Pour récupérer / lire la valeur d'un attribut
+     */
+    public function getId() {
+        return $this->_id;
+    }
+    
+    public function getNom() {
+        return $this->_nom;
+    }
+    
+    public function getDegats() {
+        return $this->_degats;
+    }
+    
+    
+     /*
+      * Methodes Mutateurs (Setters) - Pour modifier la valeur d'un attribut
+      */
+     public function setId($id) {
+         $this->_id = (int)$id; // Pas de vérification - ID est obligatoirement un entier strictement positif
+     }
+     
+     public function setNom($nom) {
+         if (is_string($nom)) {     // Vérification si présence d'une chaîne de caractères
+             $this->_nom = $nom;    // On assigne alors la valeur $nom à l'attribut _nom
+         }
+     }
+     
+     public function setDegats($degats) {
+         $degats = (int)$degats; // Conversion de l'argument en nombre entier
+         // Vérification - Le nombre doit être strictemeznt positif et compris entre 0 et 100
+         if ($degats >= 0 && $degats <= 100) {
+             $this->_degats = $degats; // on assigne alors la valeur $degats à l'attribut _degats
+         }
+     }
+    
+}
